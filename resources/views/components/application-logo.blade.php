3db616af340e380426923b5a098f320de1040dc1
paqgamer <p.a.qgamer@gmail.com>
2026-08-09 20:00:21 -0300

    feat: adiciona breeze e tela de login

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-black p-3 rounded-lg inline-block">
+    <img
+        src="{{ asset('image/alogodesafio.png') }}"
+        class="w-32"
+        alt="De$afio"
+    >
+    {{-- que jeito desgraçado que o laravel usa pra botar a logo na tela
+    vou deixar o "estilo" aqui mesmo, vontade de assassinar quem criou esse tal de tailwind--}}
+</div>
